c43e43b750e8a2d2bd4c11a019797e00ebac53c9
a.afanasiev <afanasiev.aleksandr.92@gmail.com>
2016-06-30 16:21:19 +0300

    update files

--- app/src/main/java/com/aleksandr/weathering/utils/Constants.java
+++ app/src/main/java/com/aleksandr/weathering/utils/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
     public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
     public static final String MODE = "json";
     public static final int COUNT = 15;
+    public static final int TIME_IN_MILLISECONDS = 1000;
+    public static final int SECONDS_IN_DAY = 86_400;
     public static final String IDS = "efcd21a8f54d97e56e01daa36cd6a4ce";
 
 }
